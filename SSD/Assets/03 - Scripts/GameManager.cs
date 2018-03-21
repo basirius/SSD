0b0fa4190f792ad02452723b70c38cfde6952a86
Manuel Basiri <basirius@gmail.com>
2018-03-21 18:31:11 +1100

    Singelton

---
+++ SSD/Assets/03 - Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+
+    // We can have singleton or persistant singleton. 
+    // to access the GameManager you do GameManager.Instance
+}
