efdc85de70e32669803fc7ff55de92aaf4a60184
Manuel Basiri <basirius@gmail.com>
2018-03-22 22:08:18 +1100

    Working on the State Machine

---
+++ SSD/Assets/03 - Scripts/CubeBoyScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeBoyScript : MonoBehaviour {
+
+    private StateMachine stateMachine = new StateMachine();
+    [SerializeField]
+    private LayerMask foodItemLayer;
+    [SerializeField]
+    private float viewRange;
+    
+    // Continue from minute 19
+}
---
+++ SSD/Assets/03 - Scripts/States/SearchFor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SearchFor : IState
+{
+    private LayerMask searchLayer;
+    private GameObject owner;
+    private float searchRadius;
+    private string tagToSearchFor;
+    // Not part of the main State
+    private NavMeshAgent navMeshAgent;
+
+    public SearchFor(LayerMask searchLayer, GameObject owner, float searchRadius, string tagToSearchFor, NavMeshAgent navMeshAgent)
+    {
+        this.searchLayer = searchLayer;
+        this.owner = owner;
+        this.searchRadius = searchRadius;
+        this.tagToSearchFor = tagToSearchFor;
+        this.navMeshAgent = navMeshAgent;
+    }
+
+
+    public void Enter()
+    {
+
+    }
+
+    public void Execute()
+    {
+        var hitObjects = Physics.OverlapSphere(this.owner.transform.position, searchRadius);
+        for (int i = 0; i < hitObjects.Length; i++)
+        {
+            if (hitObjects[i].CompareTag(tagToSearchFor))
+            {
+                // Actually the best way is to return the found objects however in this demo we move to the found object.
+                // The reference to the Nav Mesh Agent is also not part of the main funtionality
+                this.navMeshAgent.SetDestination(hitObjects[i].transform.position);
+            }
+            break;
+        }
+    }
+
+    public void Exit()
+    {
+
+    }
+}
