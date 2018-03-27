bbdbab3402c797070ba4d8c7cf81e09398f272de
Manuel Basiri <basirius@gmail.com>
2018-03-27 18:28:05 +1100

    Some progress

--- SSD/Assets/03 - Scripts/Managers/GameManager.cs
+++ SSD/Assets/03 - Scripts/Managers/GameManager.cs
@@ -7,12 +7,13 @@ public class GameManager : UnitySingletonPersistent<GameManager> {
 
     // This is the main persistent singleton Game Manager
     // to use this in any script in the game gameManager = GameManager.Instance;
-    
+    public float TunnelSpawnSpeed;
 
     void Start()
     {
         Cursor.visible = true;
         SceneManager.LoadScene("01 - Scenes/00 - StartMenu");
+        TunnelSpawnSpeed = 240;
     }
 
     void Update()
---
+++ SSD/Assets/03 - Scripts/TunnelBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelBehaviour : MonoBehaviour {
+
+    public float MovementSpeed;
+    GameManager gameManager = GameManager.Instance;
+
+	void Start () {
+        MovementSpeed = gameManager.TunnelSpawnSpeed;
+        print(MovementSpeed);
+	}
+	
+	
+	void Update () {
+		
+	}
+}
