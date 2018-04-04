f06dc96e9b928a206df4af00072854627dcf598a
Manuel Basiri <basirius@gmail.com>
2018-04-04 22:52:20 +1000

    beginning health bar

---
+++ SSD/Assets/03 - Scripts/Managers/LevelUIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUIManager : UnitySingleton<LevelUIManager> {
+
+    public Image CurrentShieldBar;
+    private float currentShield = 150;
+    private float maximumShield = 150;
+
+	void Start () {
+        GameManager gameManager = GameManager.Instance;
+
+    }
+	
+	void Update () {
+		
+	}
+
+    private void UpdateUI()
+    {
+
+    }
+}
